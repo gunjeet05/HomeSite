97833294f9d08fffa36a0866d6bc99791bfe0f22
gunjeet05 <gunjeet.singh9971@gmail.com>
2024-07-01 15:08:47 +0530

    Routing

--- HomeListing/src/App.jsx
+++ HomeListing/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
-  return <h1 className="">console.log("Hello world");</h1>;
+  return <Router>
+   <Routes>
+   <Route path="/"  element={<Home/>}/> 
+   <Route path="/Login"  element={<Login/>}/>
+   <Route path="/signup"  element={<SignUp/>}/> 
+   <Route path="/profile"  element={<Profile/>}/> 
+   <Route path="/about"  element={<About/>}/> 
+
+   </Routes>
+  </Router>;
   
 };
 
